fd0f7f68726266cc8f1af6e40e84ddc9e660da91
Caique <caique.tavares@bitize.com.br>
2025-03-28 10:01:15 -0300

    add: implementar componentes de seção de vídeo; criar SubscriptionButton, UserInfo, VideoBanner, VideoOwner, VideoTopRow e integrá-los em VideoSection

---
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { VideoGetOneOutput } from "../../type";
+import { UserAvatar } from "@/components/user-avatar";
+import { Button } from "@/components/ui/button";
+import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { useAuth } from "@clerk/nextjs";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
+
+interface VideoOwnerProps {
+  user: VideoGetOneOutput["user"];
+  videoId: string;
+}
+
+export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
+  const { userId: clerkUserId } = useAuth(); // Replace with actual user ID retrieval logic
+  return (
+    <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3">
+      <Link href={`/users/${user.id}`}>
+        <div className="flex items-center gap-3 min-w-0">
+          <UserAvatar size="lg" imageUrl={user.imageUrl} name={user.name} />
+          <UserInfo size="lg" name={user.name} />
+          <span className="text-sm text-muted-foreground line-clamp-1">
+            {0} subscribers
+          </span>
+        </div>
+      </Link>
+      {clerkUserId === user.clerkId ? (
+        <Button className="rounded-full" asChild variant="secondary">
+          <Link href={`/studio/videos/${videoId}`}>Edit Video</Link>
+        </Button>
+      ) : (
+        <SubscriptionButton
+          onClick={() => {}}
+          disabled={false}
+          isSubscribed={false}
+          className="rounded-full"
+          size="sm"
+        />
+      )}
+    </div>
+  );
+};
